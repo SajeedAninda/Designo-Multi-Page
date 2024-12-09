0d9ba478c718671a193b60a66d90ea1f5cc55eba
SajeedAninda <sajaninda@gmail.com>
2024-12-09 13:51:52 +0600

    Added hover effect on images

--- src/Components/Homepage/ProjectGallery/ProjectGallery.jsx
+++ src/Components/Homepage/ProjectGallery/ProjectGallery.jsx
@@ -6,18 +6,62 @@ import graphicsDesignImg from "../../../assets/home/desktop/graphic-design.jpg";
 const ProjectGallery = () => {
     return (
         <div className='w-[1000px] mx-auto mt-32 flex items-center gap-6 mb-20'>
-            <div className='flex-1 h-[640px]'>
-                <div className="webDesign h-full">
-                    <img className='w-full h-full rounded-2xl object-cover' src={webDesignImg} alt="" />
+            {/* Web Design */}
+            <div className='flex-1 h-[640px] relative group'>
+                <div className="webDesign h-full group">
+                    <div className="textDiv uppercase absolute flex justify-center items-center flex-col inset-0 z-10 group">
+                        <h1 className='text-white text-[40px] font-medium'>
+                            Web Design
+                        </h1>
+
+                        <p className='text-white text-[16px] tracking-widest font-medium mt-6 cursor-pointer'>
+                            View Projects
+                        </p>
+                    </div>
+                    <img
+                        className='w-full h-full rounded-2xl object-cover'
+                        src={webDesignImg}
+                        alt="Web Design"
+                    />
+                    <div className="overlay absolute inset-0 bg-slate-900 group-hover:bg-[#e88069] bg-opacity-50 group-hover:bg-opacity-80 transition duration-300 rounded-2xl"></div>
                 </div>
             </div>
 
-            <div className='flex flex-1 flex-col gap-4  h-[640px]'>
-                <div className="appDesign flex-1">
-                    <img className='w-full h-full rounded-2xl object-cover' src={appDesignImg} alt="" />
+            {/* App and Graphics Design */}
+            <div className='flex flex-1 flex-col gap-4 h-[640px]'>
+                <div className="appDesign flex-1 relative group">
+                    <div className="textDiv uppercase absolute flex justify-center items-center flex-col inset-0 z-10 group">
+                        <h1 className='text-white text-[40px] font-medium'>
+                            App Design
+                        </h1>
+
+                        <p className='text-white text-[16px] tracking-widest font-medium mt-6 cursor-pointer'>
+                            View Projects
+                        </p>
+                    </div>
+                    <img
+                        className='w-full h-full rounded-2xl object-cover'
+                        src={appDesignImg}
+                        alt="App Design"
+                    />
+                    <div className="overlay absolute inset-0 bg-slate-900 group-hover:bg-[#e88069] bg-opacity-50 group-hover:bg-opacity-80 transition duration-300 rounded-2xl"></div>
                 </div>
-                <div className="graphicsDesign flex-1">
-                    <img className='w-full h-full rounded-2xl object-cover' src={graphicsDesignImg} alt="" />
+                <div className="graphicsDesign flex-1 relative group">
+                    <div className="textDiv uppercase absolute flex justify-center items-center flex-col inset-0 z-10 group">
+                        <h1 className='text-white text-[40px] font-medium'>
+                            Graphics Design
+                        </h1>
+
+                        <p className='text-white text-[16px] tracking-widest font-medium mt-6 cursor-pointer'>
+                            View Projects
+                        </p>
+                    </div>
+                    <img
+                        className='w-full h-full rounded-2xl object-cover'
+                        src={graphicsDesignImg}
+                        alt="Graphics Design"
+                    />
+                    <div className="overlay absolute inset-0 bg-slate-900 group-hover:bg-[#e88069] bg-opacity-50 group-hover:bg-opacity-80 transition duration-300 rounded-2xl"></div>
                 </div>
             </div>
         </div>
