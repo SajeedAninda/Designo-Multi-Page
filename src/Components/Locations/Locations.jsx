a192b5c1c4bddbb5e171eb903fe8e2d915eea9b9
SajeedAninda <sajaninda@gmail.com>
2024-12-11 17:50:14 +0600

    Added UK office

--- src/Components/Locations/Locations.jsx
+++ src/Components/Locations/Locations.jsx
@@ -79,6 +79,42 @@ const Locations = () => {
             />
           </div>
         </div>
+
+        <div className='UK mt-32 flex gap-8 items-center justify-between'>
+          <div className='w-[60%] pl-8 pr-28 py-20 rounded-2xl bg-[#ffac993d] locationBg'>
+            <h2 className='text-[40px] font-medium text-[#E88069] leading-[40px] w-[60%]'>
+              United Kingdom
+            </h2>
+            <div className='flex items-center justify-between'>
+              <div>
+                <p className='mt-8 text-[#323036] text-[18px] font-bold'>
+                  Designo UK Office
+                </p>
+                <p className='mt-1 text-[#323036] text-[16px]'>
+                  13 Colorado Way <br /> Rhyd-y-fro SA8 9GA
+                </p>
+              </div>
+
+              <div>
+                <p className='mt-8 text-[#323036] text-[18px] font-bold'>
+                  Contact
+                </p>
+                <p className='mt-1 text-[#323036] text-[16px]'>
+                  P : 078 3115 1400
+                  <br />M : contact@designo.co
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className='w-[40%] flex justify-end'>
+            <img
+              className='object-contain w-full rounded-2xl'
+              src={ukImg}
+              alt=''
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
