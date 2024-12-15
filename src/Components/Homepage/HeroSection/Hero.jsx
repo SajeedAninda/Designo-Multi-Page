7ea6a8475a57907f3ef5b5e7c084ba11b5289a2a
SajeedAninda <sajaninda@gmail.com>
2024-12-15 17:20:57 +0600

    Linked learn more button with about page

--- src/Components/Homepage/HeroSection/Hero.jsx
+++ src/Components/Homepage/HeroSection/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import heroBg from "../../../assets/home/desktop/bg-pattern-hero-home.svg";
 import heroImg from "../../../assets/home/desktop/image-hero-phone.png";
+import { Link } from 'react-router-dom';
 
 
 const Hero = () => {
@@ -14,9 +15,11 @@ const Hero = () => {
                     <p className='text-[16px] text-white mt-8'>
                         With over 10 years in the industry, we are experienced in creating fully responsive websites, app design, and engaging brand experiences. Find out more about our services.
                     </p>
-                    <button className='uppercase font-medium mt-8 px-8 py-4 rounded-xl bg-white text-[#1c1b1d] hover:bg-[#ffac99] hover:text-white transition duration-500 ease-in-out'>
+                    <div className='mt-12'>
+                    <Link to={'/aboutCompany'} className='uppercase font-medium px-8 py-4 rounded-xl bg-white text-[#1c1b1d] hover:bg-[#ffac99] hover:text-white transition duration-500 ease-in-out'>
                         Learn More
-                    </button>
+                    </Link>
+                    </div>
                 </div>
                 <div className='absolute -bottom-52 -right-24'>
                     <img src={heroImg} alt="Hero Phone" />
